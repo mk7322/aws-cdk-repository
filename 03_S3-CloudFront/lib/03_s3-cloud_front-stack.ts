30ca56355818f2da7e6575dd6e53468087267fe3
sugawara <mavs4133@gmail.com>
2023-10-13 13:52:35 +0900

    S3静的ウェブサイトホスティング追加

--- 03_S3-CloudFront/lib/03_s3-cloud_front-stack.ts
+++ 03_S3-CloudFront/lib/03_s3-cloud_front-stack.ts
@@ -4,19 +4,27 @@ import * as iam from 'aws-cdk-lib/aws-iam';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
 import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
-import { BlockPublicAccess } from 'aws-cdk-lib/aws-s3';
 
 export class S3CFntStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     const webHostingBucket = new s3.Bucket(this, 'WebHostingBucket', {
+      // bucketName: XXXXX,
       removalPolicy: cdk.RemovalPolicy.DESTROY, // テストや開発目的の場合のみ使用する
-      autoDeleteObjects: true,
+      autoDeleteObjects: true, // デフォルトはfalse
       websiteIndexDocument: 'index.html',
       websiteErrorDocument: 'error.html',
       publicReadAccess: true,
-      blockPublicAccess: BlockPublicAccess.BLOCK_ACLS, // public access時に設定
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ACLS, // public access時に設定
+      // versioned: true,
+      // encryption: , // デフォルトはKMS
+      // encryptionKey: ,
+      // cors: [],
+      // intelligentTieringConfigurations: []
+      // lifecycleRules: [],
+      // enforceSSL: true,
+      // minimumTLSVersion: ,
     });
 
     // 公開リードアクセスのためのバケットポリシーを追加
