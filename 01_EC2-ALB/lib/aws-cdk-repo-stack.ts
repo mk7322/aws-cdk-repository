adf7b62a3d39936a5e0080ae8b8734a8b8ea5eb2
sugawara <mavs4133@gmail.com>
2023-10-14 06:10:42 +0900

    不要なコードとコメントアウトを削除

--- 01_EC2-ALB/lib/aws-cdk-repo-stack.ts
+++ 01_EC2-ALB/lib/aws-cdk-repo-stack.ts
@@ -13,42 +13,38 @@ export class AwsCdkRepoStack extends cdk.Stack {
 
 		/* ALB - EC2 構成 */
 		// VPCやサブネット、エンドポイント
-		// const network = new Network(this, "Network");
+		const network = new Network(this, "Network");
 
-		// // SG
-		// const securityGroup = new Sg(this, "SecurityGroup", {
-		// 	vpc: network.vpc,
-		// });
+		// SG
+		const securityGroup = new Sg(this, "SecurityGroup", {
+			vpc: network.vpc,
+		});
 
-		// // S3
-		// const s3 = new S3(this, "ALBLogBucket", {
-		// 	vpc: network.vpc,
-		// });
+		// S3
+		const s3 = new S3(this, "ALBLogBucket", {
+			vpc: network.vpc,
+		});
 
-		// // EC2
-		// const ec2 = new Ec2(this, "EC2", {
-		// 	vpc: network.vpc,
-		// 	priSub01: network.priSub01,
-		// 	priSub02: network.priSub02,
-		// 	// instanceCount: 6,
-		// 	ec2Sg: securityGroup.ec2Sg,
-		// });
+		// EC2
+		const ec2 = new Ec2(this, "EC2", {
+			vpc: network.vpc,
+			priSub01: network.priSub01,
+			priSub02: network.priSub02,
+			// instanceCount: 6,
+			ec2Sg: securityGroup.ec2Sg,
+		});
 
-		// // ALB
-		// const alb = new Alb(this, "LB", {
-		// 	vpc: network.vpc,
-		// 	pubSub01: network.pubSub01,
-		// 	pubSub02: network.pubSub02,
-		// 	// instanceCount: 6,
-		// 	instance01: ec2.instance01,
-		// 	instance02: ec2.instance02,
-		// 	albSg: securityGroup.albSg,
-		// 	albLogBucket: s3.albLogBucket,
-		// });
-
-
-		/* CloudFront - S3 構成 */
-
+		// ALB
+		const alb = new Alb(this, "LB", {
+			vpc: network.vpc,
+			pubSub01: network.pubSub01,
+			pubSub02: network.pubSub02,
+			// instanceCount: 6,
+			instance01: ec2.instance01,
+			instance02: ec2.instance02,
+			albSg: securityGroup.albSg,
+			albLogBucket: s3.albLogBucket,
+		});
 
 	}
 }
